f7765fd89d466cb5f2262e7bc9e66fbc961d3809
rohinivsenthil <rohinivsenthil@gmail.com>
2020-01-30 16:10:13 +0530

    Updated route tests

---
+++ tests/router/routes.test.js
@@ -0,0 +1,41 @@
+const {init} = require('../../src/server');
+const dbOperations = require('../../src/utils/dbOperations');
+
+
+describe('the server,', () => {  
+	
+	let server;
+
+	beforeEach(async () => {
+		server = await init();
+	});
+
+	afterEach(async () => {
+		await server.stop();
+	});
+    
+	xit ('should return a statusCode 200 when /shortens is hit with POST ', async () => {
+		const options = {
+			method: 'POST',
+			url: '/shortens',
+			payload: 'longURL'
+		};
+		const mockstoreURLToDB = jest.spyOn(dbOperations, 'storeURLToDB');
+		mockstoreURLToDB.mockResolvedValue();
+		const response = await server.inject(options);
+		expect(response.statusCode).toBe(200);
+	});
+    
+	it ('should return a statusCode 200 when /{shortPath} is hit with GET ', async () => {
+		const options = {
+			method: 'GET',
+			url: '/shortPath',
+		};
+		const mockstoreURLToDB = jest.spyOn(dbOperations, 'getLongURLFromDB');
+		const mockGetLongURLFromDBResponse = [{longURL: 'longURL'}];
+		mockstoreURLToDB.mockResolvedValue(mockGetLongURLFromDBResponse[0].longURL);
+		const response = await server.inject(options);
+		expect(response.statusCode).toBe(200);
+	});
+
+});
